d147fd75d6ad713d333f525b6035439e51e23852
Charisse Underwood <charisseunderwood@hotmail.com>
2019-05-08 08:34:57 -0700

    Add files via upload

---
+++ Python/Fundamentals/functions_intermediate.py
@@ -0,0 +1,27 @@
+# should print a random integer between 0 to 100
+import random
+def randInt(min=0, max=100):
+    num = random.randrange(0,100)
+    return num
+print(randInt(0,14)) 
+
+# should print a random integer between 0 to 50
+import random
+def randInt(min=0, max=50):
+    num = random.randomrange(0,50)
+    return num
+print(randInt(max=50)) 
+
+# should print a random integer between 50 to 100import random
+def randInt(min=50, max=100):
+    num = random.randrange(50,100)
+    return num
+print(randInt(min=50))
+
+#print(randInt(min=50, max=500))    # should print a random integer between 50 and 500import random
+
+def randInt(min=50, max=500):
+    num = random.randrange(50,500)
+    return num
+
+
